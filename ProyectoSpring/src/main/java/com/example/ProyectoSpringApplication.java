f4635ddb5fefbe803426cdace27046493b2e16ef
ruben <rubenrc233@gmail.com>
2024-06-18 09:07:32 +0200

    First propperties configuration

--- ProyectoSpring/src/main/java/com/example/ProyectoSpringApplication.java
+++ ProyectoSpring/src/main/java/com/example/ProyectoSpringApplication.java
@@ -1,13 +1,20 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoSpringApplication {
+public class ProyectoSpringApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println("Aplicación arrancada......");
+		
 	}
 
 }
