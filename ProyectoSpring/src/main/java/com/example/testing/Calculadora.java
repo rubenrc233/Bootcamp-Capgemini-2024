ccf293d2bb1e052d1b58c2a390f3eecaa4f738df
ruben <rubenrc233@gmail.com>
2024-06-19 12:39:32 +0200

    update test cases

--- ProyectoSpring/src/main/java/com/example/testing/Calculadora.java
+++ ProyectoSpring/src/main/java/com/example/testing/Calculadora.java
@@ -1,8 +1,26 @@
 package com.example.testing;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Calculadora {
 
+	private double redondea(double value) {
+		return new BigDecimal(value).setScale(16,RoundingMode.HALF_UP).doubleValue();
+	}
 	double add(double a, double b) {
-		return a + b;
+		return redondea(a + b);
+	}
+	double div(double a, double b) {
+		if(b == 0) {
+			throw new ArithmeticException(" / by 0");
+		}
+		return redondea(a / b);
+	}
+	double div(int a, int b) {
+		if(b == 0) {
+			throw new ArithmeticException(" / by 0");
+		}
+		return a / b;
 	}
 }
--- ProyectoSpring/src/test/java/com/example/testing/CalculadoraTest.java
+++ ProyectoSpring/src/test/java/com/example/testing/CalculadoraTest.java
@@ -27,10 +27,28 @@ class CalculadoraTest {
 	}
 
 	@Test
-	void test() {
+	void testAdd() {
 		var cal = new Calculadora();
 		var calculation = cal.add(2, 2);
-		assertEquals(4, 4);
+		assertEquals(4, calculation);
+	}
+	@Test
+	void testAddRealValues() {
+		var cal = new Calculadora();
+		var calculation = cal.add(1, -0.9);
+		assertEquals(0.1, calculation);
+	}
+	
+	@Test
+	void testDiv() {
+		var cal = new Calculadora();
+		var calculation = cal.div(3.0, 2.0);
+		assertEquals(1.5, calculation);
+	}
+	@Test
+	void testDivError() {
+		var cal = new Calculadora();
+		assertThrows(ArithmeticException.class, () -> cal.div(3.0,0));
 	}
 
 }
