003f4eb00e90ca7bcb3069ab23580c344ced5aba
ruben <rubenrc233@gmail.com>
2024-06-24 12:32:02 +0200

    Spring data first approach

--- ProyectoSpring/src/main/java/com/example/ProyectoSpringApplication.java
+++ ProyectoSpring/src/main/java/com/example/ProyectoSpringApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.ioc.Entorno;
-import com.example.ioc.Range;
-import com.example.ioc.Saluda;
+import com.example.domains.contracts.repositories.ActorRepository;
+import com.example.domains.entities.Actor;
 
 @SpringBootApplication
 public class ProyectoSpringApplication implements CommandLineRunner{
@@ -16,9 +15,22 @@ public class ProyectoSpringApplication implements CommandLineRunner{
 		SpringApplication.run(ProyectoSpringApplication.class, args);
 	}
 
+	@Autowired
+	ActorRepository daoActorRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		var item = daoActorRepository.findById(202);
+		if(item.isPresent()) {
+			System.out.println("Actor encontrado: " + item.toString());
+		} else {
+			System.out.println("Actor no encontrado");
+		}
+		
+		//var actor = new Actor(0,"Jose","Gonzalez");
+		//System.out.println("Actor guardado: "+ daoActorRepository.save(actor));
+		
+		daoActorRepository.findAll().forEach(ac -> System.out.println(ac.toString()));
 		
 	}
 
