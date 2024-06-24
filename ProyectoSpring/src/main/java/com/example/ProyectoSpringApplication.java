bc265f08c8c6a6829d18dc8f774868ee5b17ed6e
ruben <rubenrc233@gmail.com>
2024-06-24 09:58:57 +0200

    added JPA and clear application run

--- ProyectoSpring/src/main/java/com/example/ProyectoSpringApplication.java
+++ ProyectoSpring/src/main/java/com/example/ProyectoSpringApplication.java
@@ -15,25 +15,12 @@ public class ProyectoSpringApplication implements CommandLineRunner{
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringApplication.class, args);
 	}
-	
-	@Autowired
-	Saluda saluda;
-	@Autowired
-	Saluda saluda2;
-	@Autowired
-	Entorno entorno;
-	@Autowired
-	Range range;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.err.println("Aplicación arrancada......");
-		saluda.saluda("Mundo");
-		saluda2.saluda("Mundo");
-		saluda.saluda("Mundo");
-		System.out.println(entorno.getCount());
-		System.out.println("Min range: " + range.getMin() + " --> " + "Max range: " + range.getMax());
-
+		// TODO Auto-generated method stub
+		
 	}
+
 
 }
