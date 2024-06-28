a76113012ae9dc5b0d8ac769d1dd93665a8e48e4
ruben <rubenrc233@gmail.com>
2024-06-28 13:13:15 +0200

    Operations added to the endpoint

--- demoSoap/src/main/java/com/example/application/endpoints/CalculatorEndpoint.java
+++ demoSoap/src/main/java/com/example/application/endpoints/CalculatorEndpoint.java
@@ -7,17 +7,46 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import com.example.webservices.schemas.calculator.AddRequest;
 import com.example.webservices.schemas.calculator.AddResponse;
+import com.example.webservices.schemas.calculator.SubtractRequest;
+import com.example.webservices.schemas.calculator.SubtractResponse;
+import com.example.webservices.schemas.calculator.MultiplyRequest;
+import com.example.webservices.schemas.calculator.MultiplyResponse;
+import com.example.webservices.schemas.calculator.DivideRequest;
+import com.example.webservices.schemas.calculator.DivideResponse;
 
 @Endpoint
 public class CalculatorEndpoint {
-	private static final String NAMESPACE_URI = "http://example.com/webservices/schemas/calculator";
+    private static final String NAMESPACE_URI = "http://example.com/webservices/schemas/calculator";
 
-	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "addRequest")
-	@ResponsePayload
-	public AddResponse add(@RequestPayload AddRequest request) {
-		var result = new AddResponse();
-		result.setAddResult(request.getOp1() + request.getOp2());
-		return result;
-	}
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "addRequest")
+    @ResponsePayload
+    public AddResponse add(@RequestPayload AddRequest request) {
+        var result = new AddResponse();
+        result.setAddResult(request.getOp1() + request.getOp2());
+        return result;
+    }
 
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "subtractRequest")
+    @ResponsePayload
+    public SubtractResponse subtract(@RequestPayload SubtractRequest request) {
+        var result = new SubtractResponse();
+        result.setSubtractResult(request.getOp1() - request.getOp2());
+        return result;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "multiplyRequest")
+    @ResponsePayload
+    public MultiplyResponse multiply(@RequestPayload MultiplyRequest request) {
+        var result = new MultiplyResponse();
+        result.setMultiplyResult(request.getOp1() * request.getOp2());
+        return result;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "divideRequest")
+    @ResponsePayload
+    public DivideResponse divide(@RequestPayload DivideRequest request) {
+        var result = new DivideResponse();
+        result.setDivideResult(request.getOp1() / request.getOp2());
+        return result;
+    }
 }
