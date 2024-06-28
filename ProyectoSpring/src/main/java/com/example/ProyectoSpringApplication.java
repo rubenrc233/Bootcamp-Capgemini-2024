2d4ddfa2a0997ab56635dc828653f5c0129b1008
ruben <rubenrc233@gmail.com>
2024-06-28 13:47:16 +0200

    remove warnings

--- ProyectoSpring/src/main/java/com/example/ProyectoSpringApplication.java
+++ ProyectoSpring/src/main/java/com/example/ProyectoSpringApplication.java
@@ -4,20 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.ws.client.core.WebServiceTemplate;
-import org.springframework.ws.soap.client.core.SoapActionCallback;
-
-import com.example.application.proxies.CalculatorProxyImpl;
 import com.example.domains.contracts.proxies.CalculatorProxy;
-import com.example.webservice.schema.AddRequest;
-import com.example.webservice.schema.AddResponse;
-import com.example.webservice.schema.DivideRequest;
-import com.example.webservice.schema.DivideResponse;
-import com.example.webservice.schema.MultiplyRequest;
-import com.example.webservice.schema.MultiplyResponse;
-import com.example.webservice.schema.SubtractRequest;
-import com.example.webservice.schema.SubtractResponse;
 
 @SpringBootApplication
 public class ProyectoSpringApplication implements CommandLineRunner{
