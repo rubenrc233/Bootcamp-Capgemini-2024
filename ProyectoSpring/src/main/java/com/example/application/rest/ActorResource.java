92fd3874f0db7e755691a769b81c7c5b2d4c5de2
ruben <rubenrc233@gmail.com>
2024-07-01 12:04:54 +0200

    actor controller methods

--- ProyectoSpring/src/main/java/com/example/application/rest/ActorResource.java
+++ ProyectoSpring/src/main/java/com/example/application/rest/ActorResource.java
@@ -3,17 +3,20 @@ package com.example.application.rest;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.domains.contracts.services.ActorService;
-import com.example.domains.entities.Actor;
 import com.example.domains.entities.models.ActorDTO;
 import com.example.domains.entities.models.ActorShort;
 import com.example.exceptions.BadRequestException;
@@ -55,5 +58,22 @@ public class ActorResource {
 		 return ResponseEntity.created(location).build();
 	 }
 
+	 @PutMapping(path = "/{id}")
+	 @ResponseStatus(HttpStatus.NO_CONTENT)
+	 public void update(@PathVariable int id, @Valid @RequestBody ActorDTO item) throws BadRequestException, NotFoundException, InvalidDataException {
+		 if(id != item.getActorId()) {
+			 throw new BadRequestException("Los ids no coinciden");	
+		 }
+		 service.modify(ActorDTO.from(item));
+		 
+	 }
+	 
+	 @DeleteMapping(path = "/{id}")
+	 @ResponseStatus(HttpStatus.NO_CONTENT)
+	 public void delete(@PathVariable int id) {
+		 service.deleteById(id);
+		 
+	 }
+	 
 }
 
