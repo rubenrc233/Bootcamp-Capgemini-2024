5c169ce9c56d29b4e7be24a39ebde00fe4d33e0c
ruben <rubenrc233@gmail.com>
2024-07-01 11:52:45 +0200

    added service and dtos

--- ProyectoSpring/src/main/java/com/example/application/rest/ActorResource.java
+++ ProyectoSpring/src/main/java/com/example/application/rest/ActorResource.java
@@ -3,11 +3,15 @@ package com.example.application.rest;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domains.contracts.services.ActorService;
 import com.example.domains.entities.Actor;
+import com.example.domains.entities.models.ActorDTO;
+import com.example.domains.entities.models.ActorShort;
+import com.example.exceptions.NotFoundException;
 
 @RestController
 @RequestMapping("/api/actors/v1")
@@ -20,8 +24,18 @@ public class ActorResource {
 	}
 	
 	 @GetMapping
-	 public List<Actor> getAll(){
-		 return srv.getByProjection(ActorShort.class)
+	 public List<ActorShort> getAll(){
+		 return service.getByProjection(ActorShort.class);
+	 }
+	 
+	 @GetMapping(path = "/{id}")
+	 public ActorDTO getOne(@PathVariable int id) throws NotFoundException{
+		 var item = service.getOne(id);
+		 if(item.isEmpty()) {
+			 throw new NotFoundException();
+		 }
+		 return ActorDTO.from(item.get());
+		 
 	 }
 
 }
