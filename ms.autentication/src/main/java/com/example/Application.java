6437547ddf882010a0dd6a3429fa590ced1ca207
ruben <rubenrc233@gmail.com>
2024-07-05 11:57:15 +0200

    minor update

--- ms.autentication/src/main/java/com/example/Application.java
+++ ms.autentication/src/main/java/com/example/Application.java
@@ -13,15 +13,6 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Microservicio: Autenticación",
-                version = "1.0",
-                description = "Ejemplo de servidor de autenticación.",
-                license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html"),
-                contact = @Contact(name = "Javier Martín", url = "https://github.com/jmagit", email = "support@example.com")
-        ),
-        externalDocs = @ExternalDocumentation(description = "Documentación del proyecto", url = "https://github.com/jmagit/REM20220725"))
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 @SpringBootApplication
 public class Application {
