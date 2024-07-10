c5ac051dd9af262d0f81fd21d980e5665958bc7e
ruben <rubenrc233@gmail.com>
2024-07-10 14:13:58 +0200

    first notification extension

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SecurityModule } from './security';
 import{LoggerService, MyCoreModule} from '@my/core'
-import { NotificationComponent } from '@main/notification';
+import { NotificationModalComponent } from '@main/notification';
 import { DemosComponent } from '@demos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SecurityModule,MyCoreModule,NotificationComponent,DemosComponent],
+  imports: [RouterOutlet,SecurityModule,MyCoreModule,NotificationModalComponent,DemosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ curso/src/app/main/notification-modal/notification-modal.component.ts
@@ -0,0 +1,16 @@
+import { NgClass, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { Component } from '@angular/core';
+import { NotificationService } from '@commonServices';
+
+@Component({
+  selector: 'app-notification-modal',
+  standalone: true,
+  imports: [NgIf,NgClass,NgFor,NgSwitch,NgSwitchCase,NgSwitchDefault],
+  templateUrl: './notification-modal.component.html',
+  styleUrl: './notification-modal.component.css'
+})
+export class NotificationModalComponent {
+  constructor(private vm: NotificationService) { }
+  public get VM() { return this.vm; }
+
+}
