04bc70cca77a7607f8607f45f5abd48fe90437c1
ruben <rubenrc233@gmail.com>
2024-07-11 09:32:34 +0200

    add notification system yo home component

--- curso/src/app/main/home/home.component.ts
+++ curso/src/app/main/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NotificationComponent } from "../notification/notification.component";
+import { NotificationModalComponent } from '../notification-modal/notification-modal.component';
+import { DemosComponent } from "../../demos/demos.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NotificationComponent, NotificationModalComponent, DemosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
