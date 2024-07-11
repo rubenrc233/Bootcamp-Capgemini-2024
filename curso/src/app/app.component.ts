680aba7681b9447002e8444c31bf57e140aceea7
ruben <rubenrc233@gmail.com>
2024-07-11 09:29:37 +0200

    created home component

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -2,16 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SecurityModule } from './security';
 import{MyCoreModule} from '@my/core'
-import { NotificationModalComponent } from '@main/notification';
+import { HomeComponent, NotificationModalComponent } from '@main';
 import { DemosComponent } from '@demos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SecurityModule,MyCoreModule,NotificationModalComponent,DemosComponent],
+  imports: [RouterOutlet,SecurityModule,MyCoreModule,NotificationModalComponent,DemosComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'curso';
-}
+export class AppComponent {}
