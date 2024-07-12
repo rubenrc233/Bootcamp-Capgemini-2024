ec14d3535df8ff8594512de9cee448ca29e38cbf
ruben <rubenrc233@gmail.com>
2024-07-12 11:12:15 +0200

    base website components

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -5,11 +5,14 @@ import{MyCoreModule} from '@my/core'
 import { HomeComponent, NotificationModalComponent } from '@main';
 import { DemosComponent } from '@demos';
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HeaderComponent } from "./main/header/header.component";
+import { NavbarComponent } from "./main/navbar/navbar.component";
+import { FooterComponent } from "./main/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SecurityModule, MyCoreModule, NotificationModalComponent, DemosComponent, HomeComponent, DashboardComponent],
+  imports: [RouterOutlet, SecurityModule, MyCoreModule, NotificationModalComponent, DemosComponent, HomeComponent, DashboardComponent, HeaderComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
