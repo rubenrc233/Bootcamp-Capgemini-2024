b824775fed4d5ef36b60dd513995d34c5614a5c9
ruben <rubenrc233@gmail.com>
2024-07-15 09:47:13 +0200

    forms

--- curso/src/app/formulario/formulario.component.ts
+++ curso/src/app/formulario/formulario.component.ts
@@ -1,13 +1,43 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule ],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  modo: 'add' | 'edit' = 'add'
+  elemento: any = { id: 0, nombre: 'Pepito', apellidos: 'Grillo', correo: 'pgrillo@example.com', edad: 99, fecha: '2024-07-15', conflictivo: true }
 
-  element = {id: 0, name:'Luis', surnames: 'Gonzalez', email:'exaple@example.com',age: 99, date:'2024/07/15', violent: true}
+  add() {
+    this.elemento = {}
+    this.modo = 'add'
+  }
+  edit(key: number) {
+    this.elemento = { id: key, nombre: 'Pepito', apellidos: 'Grillo', correo: 'pgrillo@example.com', edad: 99, fecha: '2024-07-15', conflictivo: true }
+    this.modo = 'edit'
+  }
+
+  cancel() {
+
+  }
+
+  send() {
+    switch(this.modo) {
+      case 'add':
+        window.alert(`POST: ${JSON.stringify(this.elemento)}`)
+        this.cancel()
+        break
+      case 'edit':
+        window.alert(`PUT: ${JSON.stringify(this.elemento)}`)
+        this.cancel()
+        break
+    }
+  }
 }
