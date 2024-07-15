a078bfa71ccbf7752d052ed404c8de48bbe448a4
ruben <rubenrc233@gmail.com>
2024-07-15 10:04:11 +0200

    updated form

--- curso/src/app/formulario/formulario.component.ts
+++ curso/src/app/formulario/formulario.component.ts
@@ -1,19 +1,20 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ErrorMessagePipe } from '@my/core';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [CommonModule, FormsModule ],
+  imports: [CommonModule, FormsModule, ErrorMessagePipe, ],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
   modo: 'add' | 'edit' = 'add'
-  elemento: any = { id: 0, nombre: 'Pepito', apellidos: 'Grillo', correo: 'pgrillo@example.com', edad: 99, fecha: '2024-07-15', conflictivo: true }
+  elemento: any = { }
 
   add() {
     this.elemento = {}
