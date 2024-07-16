791fd35a1692145dcf1c8cef6d744b4a110ce91c
ruben <rubenrc233@gmail.com>
2024-07-16 11:02:29 +0200

    create front project

--- curso/src/app/app.routes.ts
+++ curso/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { ContactosListComponent,ContactosAddComponent,ContactosEditComponent,ContactosViewComponent } from './contactos/componente/componente.component';
 
-export const routes: Routes = [ { path: 'contactos', children: [
+export const routes: Routes = [ 
+    { path: 'contactos', children: [
     { path: '', component: ContactosListComponent},
     { path: 'add', component: ContactosAddComponent},
     { path: ':id/edit', component: ContactosEditComponent},
